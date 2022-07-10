b5b4935d09f1462e70fde4832777ddcefbcbcbb2
thdevss <14devlab@gmail.com>
2022-07-10 10:54:05 +0700

    first commit, and migration script

---
+++ database/migrations/2022_07_10_032350_create_jobs_table.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            
+            $table->string('job_position');
+            $table->integer('job_amount')->default(1);
+
+            $table->string('company_name');
+            $table->string('company_address');
+            $table->string('telephone_number');
+
+            $table->unsignedBigInteger('job_type');
+            $table->foreign('job_type')->references('id')->on('job_type');
+
+            $table->string('job_salary');
+            $table->string('job_province');
+
+            $table->string('job_gender');
+
+            $table->unsignedBigInteger('job_degree');
+            $table->foreign('job_degree')->references('id')->on('job_degree');
+
+            $table->string('job_description');
+
+
+            $table->dateTime('expired_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+};
